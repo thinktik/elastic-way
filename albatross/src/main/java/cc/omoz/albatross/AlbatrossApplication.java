b3f36aefb015c2b3e5e9cf566411f661ef12ad99
thinktik <thinktik@outlook.com>
2022-12-07 13:29:54 +0800

    1. 补充albatross

---
+++ albatross/src/main/java/cc/omoz/albatross/AlbatrossApplication.java
@@ -0,0 +1,13 @@
+package cc.omoz.albatross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbatrossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbatrossApplication.class, args);
+    }
+
+}
