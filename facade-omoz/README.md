a4660ad2c73f42f237a27add97799ee0e75d66a9
thinktik <thinktik@outlook.com>
2022-12-23 14:55:45 +0800

    1. 补充facade-omoz

---
+++ facade-omoz/README.md
@@ -0,0 +1,7 @@
+# facade-omoz
+自定义工具组件，提供对[omoz.cc](https://www.omoz.cc/)的一些简易的包装。
+
+## misc
+### gradle打包
+- gradle 打包：`./gradlew build`
+- gradle 发布：`./gradlew publish`
---
+++ facade-omoz/src/main/java/cc/omoz/facade/dto/OmozResponse.java
@@ -0,0 +1,54 @@
+package cc.omoz.facade.dto;
+
+/**
+ * HTTP/MQTT/RPC 端点间通信的格式规范
+ *
+ * @author ThinkTik
+ */
+public abstract sealed class OmozResponse<T> permits HttpResponse {
+    /**
+     * 状态码
+     */
+    private Integer code;
+    /**
+     * 状态消息
+     */
+    private String message;
+    /**
+     * payload消息体
+     */
+    private T data;
+
+    protected OmozResponse() {
+    }
+
+    protected OmozResponse(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
