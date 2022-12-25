36e1152e44ff1127fbb77ea5669b3ce35ec8a6fc
thinktik <thinktik@outlook.com>
2022-12-25 19:59:47 +0800

    1. 补充lark

---
+++ lark/src/main/java/cc/omoz/lark/LarkApplication.java
@@ -0,0 +1,17 @@
+package cc.omoz.lark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class LarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LarkApplication.class, args);
+    }
+
+}
